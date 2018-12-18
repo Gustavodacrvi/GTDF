5dcdb86e65c169cb49d7578aea6aaba2fac1d7a1
Gustavodacrvi <gustavodacruzvieira@gmail.com>
2018-12-18 16:44:25 -0200

    Adicionado inglês e pt-BR usando i18n

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
 const path = require('path')
+const i18n = require('i18n')
 
+// I18N
+i18n.configure({
+    locales: ['en', 'pt-BR'],
+    defaultLocale: 'en',
+    queryParameter: 'lang',
+    cookie: 'langCookie',
+    directory: path.join(__dirname, 'i18n'),
+})
 
 
 var app = express()
-
+// I18N
+app.use(cookieParser())
+app.use(i18n.init)
 
 
 
@@ -14,8 +26,10 @@ app.set('view engine', 'ejs')
 
 
 
+
 // STATIC
 app.use(express.static(path.join(__dirname, 'static')))
+
 
 
 
@@ -23,13 +37,16 @@ app.use(express.static(path.join(__dirname, 'static')))
 app.get('/', function(req, res) {
     res.render('index')
 })
-
 app.get('/login', function(req, res){
     res.render('login')
+})
+app.get('/signup', function(req, res){
+    res.render('signup')
 })
 
 
 
-app.listen(3000, function(){
+
+app.listen(3000, '0.0.0.0', function(){
     console.log('Server started at port 3000...')
 })
--- static/js/loginsign.js
+++ static/js/loginsign.js
@@ -1,6 +1,37 @@
-let app = new Vue({
+let login = new Vue({
     el: '.loginBack',
     data: {
         wrong: ''
     }
 })
+
+function show(el){
+    el.css({
+        'transition-duration': '0.2s',
+        'opacity': '1',
+        'visibility': 'visible'
+    })
+}
+
+function hide(el){
+    el.css({
+        'opacity': '0',
+        'visibility': 'hidden'
+    })
+}
+
+var eye = {
+    closed: true,
+    toggleEye: function(){
+        if (!this.closed){
+            hide($('.login__open__eye'))
+            show($('.login__close__eye'))
+            $('.login__password').attr('type', 'password')
+        } else {
+            hide($('.login__close__eye'))
+            show($('.login__open__eye'))
+            $('.login__password').attr('type', 'text')
+        }
+        this.closed = !this.closed
+    }
+}
