ee39a5ecd98d4f5547d29a242158068e21471d80
Gustavodacrvi <gustavodacrvi@gmail.com>
2018-12-21 11:00:52 -0200

    Adicionado internacionalização(i18n).

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
 var express = require('express')
+var ejs = require('ejs')
+var path = require('path')
+var i18n = require('i18n')
+var cookieParser = require('cookie-parser')
+
+
+// I18N
+i18n.configure({
+    locales: ['en', 'pt-BR'],
+    cookie: 'localeCookie',
+    directory: path.join(__dirname, 'locales'),
+    queryParameter: 'lang',
+    defaultLocale: 'en',
+});
 
 var app = express()
+
+// COOKIE PARSER
+app.use(cookieParser())
+
+// I18N
+app.use(i18n.init)
+
+// VIEW ENGINE
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+// STATIC
+app.use(express.static(path.join(__dirname, 'static')))
+
+
 
 
 
 app.get('/', function(req, res){
-    res.send('Hello World')
+    res.render('index')
 })
 
 
