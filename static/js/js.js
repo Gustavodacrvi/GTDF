8e43000770a43ba819505a710afc0fd099cba60b
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-01-12 19:40:15 -0200

    userForms animation

--- static/js/js.js
+++ static/js/js.js
@@ -1,8 +1,39 @@
-new Vue({
+
+Vue.component('fixed', {
+  template: `
+    <div id='fixedElements' class='centralizeContent'>
+      <slot></slot>
+    </div>
+  `
+})
+Vue.component('login-form', {
+  props: {
+    show: Boolean
+  },
+  template: `
+    <transition name='fade'>
+      <div class='card form' v-if='show'><slot></slot></div>
+    </transition>
+  `
+})
+
+let vm = new Vue({
   el: '#app',
   data: {
-    msg: 'hey'
+    transitionsAndAnimations: {
+      userForms: {
+        show: false
+      }
+    }
+  },
+  methods: {
+    runInitialTransitionsAndAnimations(){
+      setTimeout(this.userFormsAnimation, 1000)
+    },
+    userFormsAnimation(){
+      this.transitionsAndAnimations.userForms.show = true
+    }
   }
 })
 
-console.log(3)
+vm.runInitialTransitionsAndAnimations()
