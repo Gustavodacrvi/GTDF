69acfcdce20bf29d68923ab921a960df24f48980
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-01-12 22:51:48 -0200

    Added form components and login form

--- static/js/js.js
+++ static/js/js.js
@@ -11,9 +11,62 @@ Vue.component('login-form', {
     show: Boolean
   },
   template: `
-    <transition name='fade'>
+    <transition name='double-slide-bounce'>
       <div class='card form' v-if='show'><slot></slot></div>
     </transition>
+  `
+})
+Vue.component('form-element', {
+  props: {
+    animation: String
+  },
+  template: `
+    <transition :name='animation'>
+      <div class='formElement' v-if='this.$parent.show'>
+        <slot></slot>
+      </div>
+    </transition>
+  `
+})
+Vue.component('title-big', {
+  props: {
+    class: String
+  },
+  template: `
+    <div class='class'>
+      <h1 class='titleBig'><slot></slot></h1>
+    </div>
+  `
+})
+Vue.component('input-pass', {
+  props: {
+    placeholder: String,
+    opened: false
+  },
+  template: `
+    <div class='form-input centralizeContent'>
+      <input name='password' class='passwordField' type='password' autocomplete='off' :placeholder='placeholder' />
+      <span @click='opened = !opened;$emit("click", opened)'>
+        <i class="fas fa-eye icon-tiny" v-show='opened'></i>
+        <i class="fas fa-eye-slash icon-tiny" v-show='!opened'></i>
+      <span>
+    </div>
+  `
+})
+Vue.component('input-form', {
+  props: {
+    placeholder: String,
+    name: String
+  },
+  template: `
+    <div class='form-input centralizeContent'>
+      <input :name='name' type='text' :placeholder='placeholder'/>
+    </div>
+  `
+})
+Vue.component('form-button', {
+  template: `
+    <button type='submit' class='formButton'><slot></slot></button>
   `
 })
 
@@ -28,7 +81,22 @@ let vm = new Vue({
   },
   methods: {
     runInitialTransitionsAndAnimations(){
-      setTimeout(this.userFormsAnimation, 1000)
+      setTimeout(this.userFormsAnimation, 10)
+    },
+    togglePasswordVisiblity(opened){
+      (opened) ? this.showPasswords() : this.hidePasswords()
+    },
+    showPasswords(){
+      let inputs = document.querySelectorAll('.passwordField')
+      inputs.forEach(el => {
+        el.setAttribute('type', 'text')
+      })
+    },
+    hidePasswords(){
+      let inputs = document.querySelectorAll('.passwordField')
+      inputs.forEach(el => {
+        el.setAttribute('type', 'password')
+      })
     },
     userFormsAnimation(){
       this.transitionsAndAnimations.userForms.show = true
